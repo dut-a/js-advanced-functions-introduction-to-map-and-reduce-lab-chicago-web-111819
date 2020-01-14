ab1ab5e5784c352c007c4c08197023fada4b2b02
dut-a <d.dut@live.com>
2020-01-14 13:23:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Your code here
+
+/** map-like */
+function mapToNegativize(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(element => {
+    newArray.push(element * -1);
+  });
+  return newArray;
+}
+
+function mapToNoChange(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(element => {
+    newArray.push(element);
+  });
+  return newArray;
+}
+
+function mapToDouble(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(element => {
+    newArray.push(element * 2);
+  });
+  return newArray;
+}
+
+function mapToSquare(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach(element => {
+    newArray.push(Math.pow(element, 2));
+  });
+  return newArray;
+}
+
+/** reduce-like */
+function reduceToTotal(sourceArray, startingPoint) {
+  let total = startingPoint || 0;
+  sourceArray.forEach(num => {
+    total += num;
+  });
+  return total;
+}
+
+function reduceToAllTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!sourceArray[i]) { return false; }
+  };
+  return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i]) { return true; }
+  };
+  return false;
+}
+
+
+
